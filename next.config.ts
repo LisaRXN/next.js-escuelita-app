58f318ac830791c96c256f5a316afb99d822984f
lisa erksn <lisa.eriksen@epitech.eu>
2025-07-08 21:59:03 +0200

    Restore Prisma binaries for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
+// @ts-expect-error: PrismaPlugin is not typed correctly for this usage
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
+
+
+
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack(config, { isServer }) {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
+  },
   images: {
     remotePatterns : [
       {
@@ -17,6 +28,7 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  
   
 };
 
